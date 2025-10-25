e349e764ca1647e04443da2c2f3975bbab619203
shamseer8877 <alishamseer@gmail.com>
2025-10-25 13:57:19 +0530

    create header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="sticky top-0 z-50 w-full bordrer-b bg-background/95 backdrop-blur py-2 supports-backdrop-filter:bg-background/60">
+      <div className="container mx-auto h-16 flex items-center justify-between px-4">
+        <Link to="/">
+          <img src="/logo.png" alt="Klimate Logo" className="h-14" />
+        </Link>
+        <div>
+          {/* Search */}
+          {/* Theme toggle */}
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;

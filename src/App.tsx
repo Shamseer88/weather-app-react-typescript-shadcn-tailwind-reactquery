1f027e6cf039d10fc18f20eccb66b802abba90f6
shamseer8877 <alishamseer@gmail.com>
2025-11-01 11:32:58 +0530

    create useWeather hook

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const App = () => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 5,
+        gcTime: 1000 * 60 * 10,
+        retry: false,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
